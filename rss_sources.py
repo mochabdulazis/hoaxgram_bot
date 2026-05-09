7780c4743c84c787059b89861b02cf5bc934deab
Mochamad Abdul Azis <109265521+mochabdulazis@users.noreply.github.com>
2026-05-09 09:24:54 +0700

    Create rss_sources.py

---
+++ rss_sources.py
@@ -0,0 +1,7 @@
+RSS_FEEDS = {
+    "CNN Indonesia": "https://www.cnnindonesia.com/rss",
+    "Antara": "https://www.antaranews.com/rss/terkini.xml",
+    "Tempo": "https://rss.tempo.co",
+    "Kumparan": "https://lapi.kumparan.com/v2.0/rss/",
+    "Tirto": "https://tirto.id/rss"
+}
